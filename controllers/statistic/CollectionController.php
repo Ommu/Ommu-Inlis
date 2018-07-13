3c8fbe92ac5c08367e0829e8cfdcfbb802217b55
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-13 12:54:44 +0700

    move getGridColumn function to trait

--- controllers/statistic/CollectionController.php
+++ controllers/statistic/CollectionController.php
@@ -113,15 +113,7 @@ class CollectionController extends Controller
 			$model->attributes=$_GET['ViewInlisSyncCollections'];
 		}
 
-		$columnTemp = array();
-		if(isset($_GET['GridColumn'])) {
-			foreach($_GET['GridColumn'] as $key => $val) {
-				if($_GET['GridColumn'][$key] == 1) {
-					$columnTemp[] = $key;
-				}
-			}
-		}
-		$columns = $model->getGridColumn($columnTemp);
+		$columns = $model->getGridColumn($this->gridColumnTemp());
 
 		$this->pageTitle = Yii::t('phrase', 'View Inlis Sync Collections Manage');
 		$this->pageDescription = '';
--- controllers/sync/WorksheetsubController.php
+++ controllers/sync/WorksheetsubController.php
@@ -113,15 +113,7 @@ class WorksheetsubController extends Controller
 			$model->attributes=$_GET['SyncWorksheetSub'];
 		}
 
-		$columnTemp = array();
-		if(isset($_GET['GridColumn'])) {
-			foreach($_GET['GridColumn'] as $key => $val) {
-				if($_GET['GridColumn'][$key] == 1) {
-					$columnTemp[] = $key;
-				}
-			}
-		}
-		$columns = $model->getGridColumn($columnTemp);
+		$columns = $model->getGridColumn($this->gridColumnTemp());
 
 		$this->pageTitle = Yii::t('phrase', 'Inlis Worksheet Subs Manage');
 		$this->pageDescription = '';
